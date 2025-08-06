0fd9d8b5d4551392da3ace1ac71be1cf326c71d1
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-06 16:38:11 +0530

    updates

--- 06-functions/04-fn-types.go
+++ 06-functions/04-fn-types.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -18,24 +19,30 @@ func main() {
 		fmt.Printf("Hi %s, Have a nice day!\n", userName)
 	}
 	greet("Magesh")
-	/*
-		// 2 arguments, No return results
-		func(firstName, lastName string) {
-			fmt.Printf("Hi %s %s, Have a good day!\n", firstName, lastName)
-		}("Magesh", "Kuppan")
 
-		// 2 arguments, 1 result
-		msg := func(firstName, lastName string) string {
-			return fmt.Sprintf("Hi %s %s, Have a wonderful day!", firstName, lastName)
-		}("Suresh", "Kannan")
-		fmt.Println(msg)
+	// 2 arguments, No return results
+	var greetUser func(string, string)
+	greetUser = func(firstName, lastName string) {
+		fmt.Printf("Hi %s %s, Have a good day!\n", firstName, lastName)
+	}
+	greetUser("Magesh", "Kuppan")
 
-		// 2 arguments, 2 results (named results)
-		firstName, lastName := func(fullName string) (firstName, lastName string) {
-			names := strings.Split(fullName, " ")
-			firstName, lastName = names[0], names[1]
-			return
-		}("Magesh Kuppan")
-		fmt.Printf("FirstName : %s, LastName : %s\n", firstName, lastName)
-	*/
+	// 2 arguments, 1 result
+	var getGreetMsg func(string, string) string
+	getGreetMsg = func(firstName, lastName string) string {
+		return fmt.Sprintf("Hi %s %s, Have a wonderful day!", firstName, lastName)
+	}
+	msg := getGreetMsg("Suresh", "Kannan")
+	fmt.Println(msg)
+
+	// 2 arguments, 2 results (named results)
+	var getNames func(string) (string, string)
+	getNames = func(fullName string) (firstName, lastName string) {
+		names := strings.Split(fullName, " ")
+		firstName, lastName = names[0], names[1]
+		return
+	}
+	firstName, lastName := getNames("Magesh Kuppan")
+	fmt.Printf("FirstName : %s, LastName : %s\n", firstName, lastName)
+
 }
