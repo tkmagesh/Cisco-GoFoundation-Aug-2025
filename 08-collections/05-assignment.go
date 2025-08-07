4265ccd23529025ff3cc161f81a2f6abebdefe35
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-07 12:29:08 +0530

    collection - assignment - solution

--- 08-collections/05-assignment.go
+++ 08-collections/05-assignment.go
@@ -10,3 +10,35 @@ Print the generated prime numbers in the main() function
 main -> generatedPrimes(range) -> IsPrime(no)
 */
 
+package main
+
+import "fmt"
+
+func main() {
+	var start, end int
+	fmt.Println("Enter the range :")
+	fmt.Scanln(&start, &end)
+	primes := generatePrimes(start, end)
+	for _, primeNo := range primes {
+		fmt.Printf("Prime No : %d\n", primeNo)
+	}
+}
+
+func generatePrimes(start, end int) []int {
+	primes := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primes = append(primes, no)
+		}
+	}
+	return primes
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
