a3b03378959ee0123a9c0c219dcfeed6fdcc77d4
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-07 16:39:58 +0530

    typed struct

--- 12-structs/02-typed.go
+++ 12-structs/02-typed.go
@@ -2,29 +2,23 @@ package main
 
 import "fmt"
 
+type Product struct {
+	Id   int
+	Name string
+	Cost float64
+}
+
 func main() {
 	/* product => id, name, cost */
 	/*
-		var p1 struct {
-			Id   int
-			Name string
-			Cost float64
-		}
+		var p1 Product
 		p1.Id = 100
 		p1.Name = "Pen"
 		p1.Cost = 10
 	*/
 
 	/*
-		var p1 struct {
-			Id   int
-			Name string
-			Cost float64
-		} = struct {
-			Id   int
-			Name string
-			Cost float64
-		}{
+		var p1 Product  = Product {
 			Id:   100,
 			Name: "Pen",
 			Cost: 10,
@@ -33,22 +27,14 @@ func main() {
 
 	// type inference
 	/*
-		var p1 = struct {
-			Id   int
-			Name string
-			Cost float64
-		}{
+		var p1 = Product {
 			Id:   100,
 			Name: "Pen",
 			Cost: 10,
 		}
 	*/
 
-	p1 := struct {
-		Id   int
-		Name string
-		Cost float64
-	}{
+	p1 := Product{
 		Id:   100,
 		Name: "Pen",
 		Cost: 10,
@@ -63,11 +49,7 @@ func main() {
 	fmt.Printf("p2.Cost = %v, p1.Cost = %v\n", p2.Cost, p1.Cost)
 
 	// To create a reference
-	var p1Ptr *struct {
-		Id   int
-		Name string
-		Cost float64
-	}
+	var p1Ptr *Product
 	p1Ptr = &p1
 	p1Ptr.Cost = 20
 	fmt.Printf("p1Ptr.Cost = %v, p1.Cost = %v\n", p1Ptr.Cost, p1.Cost)
@@ -85,19 +67,11 @@ func main() {
 
 // return a formatted string of the given product
 // ex: Id = 100, Name = "Pen", Cost = 10
-func Format(p struct {
-	Id   int
-	Name string
-	Cost float64
-}) string {
+func Format(p Product) string {
 	return fmt.Sprintf("Id = %d, Name = %q, Cost = %0.2f", p.Id, p.Name, p.Cost)
 }
 
 // Update the cost to apply the given discount(%) on the given product
-func ApplyDiscount(pPtr *struct {
-	Id   int
-	Name string
-	Cost float64
-}, discountPercentage float64) /* DO NOT RETURN */ {
+func ApplyDiscount(pPtr *Product, discountPercentage float64) /* DO NOT RETURN */ {
 	pPtr.Cost = pPtr.Cost * ((100 - discountPercentage) / 100)
 }
