7e9b531bdfa2d311ed43165b94bfd625446b759e
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-07 12:06:50 +0530

    collections - assignment

---
+++ 08-collections/05-assignment.go
@@ -0,0 +1,12 @@
+/*
+Accept a range from the user and print all the prime numbers between the given range
+*/
+
+/*
+Refactor the logic for generating prime nos to a "generatePrimes()" function
+Write "IsPrime()" that checks if the given number is a prime number or not
+Print the generated prime numbers in the main() function
+
+main -> generatedPrimes(range) -> IsPrime(no)
+*/
+
