e3803e7be886506dd726a750c17bc56fd0ccabd8
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-08 11:22:30 +0530

    interface composition

--- 15-interfaces/01-example.go
+++ 15-interfaces/01-example.go
@@ -60,28 +60,68 @@ func PrintArea(x interface{ Area() float64 }) {
 }
 
 // Introducing Perimeter()
-/* 
+/*
 Circle = 2 * pi * r
 Rectangle = 2 * (h + w)
 Square = 4 * side
 */
 
-func PrintPerimeter(x ?){
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (s Square) Perimeter() float64 {
+	return 4 * s.Side
+}
+
+func PrintPerimeter(x interface{ Perimeter() float64 }) {
 	fmt.Println("Perimeter :", x.Perimeter())
 }
+
+// interface composition
+func PrintStats(x interface {
+	interface{ Area() float64 }
+	interface{ Perimeter() float64 }
+}) {
+	PrintArea(x)      // x should be interface{ Area() float64}
+	PrintPerimeter(x) // x should be interface{ Perimeter() float64}
+}
+
+/*
+func PrintStats(x interface {
+	Area() float64
+	Perimeter() float64
+}) {
+	PrintArea(x)      // x should be interface{ Area() float64}
+	PrintPerimeter(x) // x should be interface{ Perimeter() float64}
+}
+*/
 
 func main() {
 	c := Circle{Radius: 12}
 	// fmt.Println("Area :", c.Area())
-	PrintArea(c)
-	PrintPerimeter(c)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintStats(c)
 
 	r := Rectangle{Height: 12, Width: 14}
 	// fmt.Println("Area :", r.Area())
-	PrintArea(r)
-	PrintPerimeter(r)
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintStats(r)
 
 	s := Square{Side: 16}
-	PrintArea(s)
-	PrintPerimeter(s)
+	/*
+		PrintArea(s)
+		PrintPerimeter(s)
+	*/
+	PrintStats(s)
 }
